04c3567820022b60ce87205a64424e4d26990aa7
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-02-25 10:22:46 +0545

    Update word_game.py
    Added hint counts and score deduction per hint

--- Word Guessing Game/word_game.py
+++ Word Guessing Game/word_game.py
@@ -1,14 +1,21 @@
 word = "Elephant"
 count = 0
 score = 0
+hint_count = 0
 print("The animal is very big, vegeterian and it's babies name is calf")
 while count <= 3:
     user_input = input("Enter your word: ")
     count += 1
     if user_input == word:
         print(f"{user_input} is correct")
-        score += 5
-        print(f"Your score is: {score}")
+        if hint_count == 0:
+            print(f"Number of hints used: {hint_count}")
+            score += 5
+            print(f"Your score is: {score}")
+        elif hint_count != 0:
+            print(f"Number of hints used: {hint_count}")
+            score += 3
+            print(f"Your score is: {score}")
         break
     elif user_input != word:
         print("Your answer is incorrect")
@@ -16,7 +23,12 @@ while count <= 3:
         if hint.upper() == "H":
             print("The animal is very big and has a trunk")
             count -= 1
+            hint_count += 1
     elif count == 3:
         print("Your tries are over!")
         score -= 5
         print(f"Your score is: {score}")
+        if hint_count == 0:
+            print(f"Number of hints used: {hint_count}. Try again later!")
+        elif hint_count != 0:
+            print(f"Number of hints used: {hint_count}. Try again later!")
