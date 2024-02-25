f873f864e28aaa99ec5aa89fa181e7b2e58120d0
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-02-25 10:14:54 +0545

    Create wordgame.py
    The game is a basic implementation of all the basic components of python.

---
+++ Word Guessing Game/word_game.py
@@ -0,0 +1,21 @@
+word = "Elephant"
+count = 0
+score = 0
+while count <= 3:
+    user_input = input("Enter your word: ")
+    count += 1
+    if user_input == word:
+        print(f"{user_input} is correct")
+        score += 5
+        print(f"Your score is: {score}")
+        break
+    elif user_input != word:
+        print("Your answer is incorrect")
+        hint = input("Press H for hint: ")
+        if hint.upper() == "H":
+            print("The animal is very big and has a trunk")
+            count -= 1
+    elif count == 3:
+        print("Your tries are over!")
+        score -= 5
+        print(f"Your score is: {score}")
