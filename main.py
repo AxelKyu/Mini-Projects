7d3857191e44f6e86631f35626882d872a14c3e3
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-02-25 09:37:45 +0545

    Added upper and lower bound
    Added the main state phase state of intervals

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import math
+
+#   initialization of upper and lower bound
+lower_bound = int(input("Enter lowerbound interval: "))
+upper_bound = int(input("Enter upperbound interval: "))
+
+#   initialization of count
+count = 0
+
