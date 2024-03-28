82a317a396221ae5a82da90f974fd4dd2adcad16
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-03-29 01:00:22 +0545

    Foundations of Qiak Media

--- hangman/main.py
+++ hangman/main.py
@@ -1,33 +1,27 @@
 import random
-
-
-tries = 0
-
-
-def hangman():
-    #   Defining the secret words
-    secret_words = ["Tranquility", "Adventure", "Wanderlust", "Discovery", "Empower", "Illuminate", "Courage"]
-    #   Choosing secret word
-    word = random.choice(secret_words)
-    tries = len(word)
-    #   Hints cases for secret words
-    if word == "Tranquility":
-        print("It is called quality or being in state of tranquil")
-    elif word == "Adventure":
-        print("Exciting or unusual thrilling experience")
-    elif word == "Wanderlust":
-        print("A strong desire to travel")
-    elif word == "Discover":
-        print("Finding something or making for the first time")
-    elif word == "Empower":
-        print("To give someone power of authority")
-    elif word == "Illuminate":
-        print("Making something visible or brighter")
-    elif word == "Courage":
-        print("The ability to do something that frightens one")
-
-    #   Getting input of the character for the game
-    user_input = input("Enter character of the word: ")
-
-
-hangman()
+game = ["rock", "paper", "scissors"]
+user_input = input("Enter rock, paper or scissors: ")
+while user_input not in game:
+    user_input = input("Enter rock, paper or scissors: ")
+bot = random.choice(game)
+if user_input == "rock" and bot == "paper":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You lost!")
+elif user_input == "rock" and bot == "scissors":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You won!")
+elif user_input == "paper" and bot == "rock":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You won!")
+elif user_input == "paper" and bot == "scissors":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You lost!")
+elif user_input == "scissors" and bot == "paper":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You won!")
+elif user_input == "scissors" and bot == "rock":
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("You lost!")
+elif user_input == bot:
+    print(f"Your choice {user_input}", f"Computers choice {bot}")
+    print("It's a draw")
