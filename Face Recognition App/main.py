6a9b6c68191d1dbc5a1f49f3bcc6693248e90d70
AxelKyu <148931255+AxelKyu@users.noreply.github.com>
2024-08-10 11:16:46 +0545

    Added check_face and the main loop
    The main code of the program is finished

--- Face Recognition App/main.py
+++ Face Recognition App/main.py
@@ -13,12 +13,31 @@ face_match = False
 
 reference_img = cv2.imread("")
 
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
 while True:
     ret, frame = cap.read()
 
     if ret:
-        pass
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
 
+        if face_match:
+            cv2.putText(frame, "MATCH", (20, 450), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH", (20, 450), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0, 0, 255), 3)
+        cv2.imshow("video", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
